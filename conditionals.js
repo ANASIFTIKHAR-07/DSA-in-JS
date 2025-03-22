a3d0b6d3fc4a842a570bcb5e94d6ddbbe1171d6d
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-03-22 13:41:07 +0500

    Updation

--- conditionals.js
+++ conditionals.js
@@ -19,7 +19,7 @@ else if (age < 18) {
     console.log("Sorry, you are not eligible to vote yet.");
     console.log("You need to wait " + (18 - age) + " more years to be eligible.");
 }
-
+ 3
 
 // Shop discount calculator
 // Get the purchase amount using prompt
@@ -35,36 +35,36 @@ if (isNaN(purchaseAmount)) {
 else if (purchaseAmount <= 0) {
     console.log("Purchase amount must be greater than 0!");
 }
-// Apply different discount rates based on purchase amount
-else if (purchaseAmount >= 25000) {
+// Apply different discount rates based on purchase amount ranges
+else if (purchaseAmount > 25000) {
     let discount = purchaseAmount * 0.30; // 30% discount
     let finalAmount = purchaseAmount - discount;
     console.log("Congratulations! You get a 30% discount.");
     console.log("Discount amount: $" + discount.toFixed(2));
     console.log("Final amount to pay: $" + finalAmount.toFixed(2));
 }
-else if (purchaseAmount >= 15000) {
+else if (purchaseAmount > 15001 && purchaseAmount <= 25000) {
     let discount = purchaseAmount * 0.25; // 25% discount
     let finalAmount = purchaseAmount - discount;
     console.log("Congratulations! You get a 25% discount.");
     console.log("Discount amount: $" + discount.toFixed(2));
     console.log("Final amount to pay: $" + finalAmount.toFixed(2));
 }
-else if (purchaseAmount >= 10000) {
+else if (purchaseAmount > 10001 && purchaseAmount <= 15000) {
     let discount = purchaseAmount * 0.20; // 20% discount
     let finalAmount = purchaseAmount - discount;
     console.log("Congratulations! You get a 20% discount.");
     console.log("Discount amount: $" + discount.toFixed(2));
     console.log("Final amount to pay: $" + finalAmount.toFixed(2));
 }
-else if (purchaseAmount >= 4000) {
+else if (purchaseAmount > 5001 && purchaseAmount <= 10000) {
     let discount = purchaseAmount * 0.15; // 15% discount
     let finalAmount = purchaseAmount - discount;
     console.log("Congratulations! You get a 15% discount.");
     console.log("Discount amount: $" + discount.toFixed(2));
     console.log("Final amount to pay: $" + finalAmount.toFixed(2));
 }
-else {
+else if (purchaseAmount > 0 && purchaseAmount <= 5000) {
     let discount = purchaseAmount * 0.05; // 5% discount
     let finalAmount = purchaseAmount - discount;
     console.log("You get a 5% discount.");
