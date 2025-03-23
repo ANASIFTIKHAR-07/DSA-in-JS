b199b8db46d73c626844ffc33a01fc7a4c7bf01d
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-03-23 22:06:51 +0500

    Loops starting

---
+++ loops.js
@@ -0,0 +1,47 @@
+// We will learn about the loops and the questions related to it.
+
+
+// Example of a for loop with increment
+console.log("For loop with increment:");
+for (let i = 0; i <= 5; i++) {
+    console.log("Current value (increment): " + i);
+}
+
+// Example of a for loop with decrement
+console.log("For loop with decrement:");
+for (let j = 5; j >= 0; j--) {
+    console.log("Current value (decrement): " + j);
+}
+
+// Example of a while loop with increment
+console.log("While loop with increment:");
+let k = 0;
+while (k <= 5) {
+    console.log("Current value (increment): " + k);
+    k++;
+}
+
+// Example of a while loop with decrement
+console.log("While loop with decrement:");
+let l = 5;
+while (l >= 0) {
+    console.log("Current value (decrement): " + l);
+    l--;
+}
+
+// Example of a do-while loop with increment
+console.log("Do-while loop with increment:");
+let m = 0;
+do {
+    console.log("Current value (increment): " + m);
+    m++;
+} while (m <= 5);
+
+// Example of a do-while loop with decrement
+console.log("Do-while loop with decrement:");
+let n = 5;
+do {
+    console.log("Current value (decrement): " + n);
+    n--;
+} while (n >= 0);
+
