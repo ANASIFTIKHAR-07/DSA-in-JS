037841805cc54acdd6f1789e7d05636df1e6f70a
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-03-24 22:09:54 +0500

    Factorial

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
- // We will learn about the loops and the questions related to it.
+// We will learn about the loops and the questions related to it.
 
 
 //  Example of a for loop with increment
@@ -49,20 +49,61 @@
 
 // sum of n natural numbers
 
-let ans = Number(prompt("How much you wanna add? :"))
 
-if (isNaN(ans)) {
-    console.log("The entered number is not valid");
+// let pr = prompt("How much you wanna add? :")
+// // I did this step because if a prompt is cancelled. It returns a null.
+
+// if (pr === null) {
+//     console.log("Prompt cancelled!!");
+// }
+// else {
+//     let ans = Number(pr)
+
+//     if (isNaN(ans)) {
+//         console.log("The entered number is not valid");
+//     }
+//     else {
+//         if (ans > 0) {
+//             let sum = 0
+//             for (i = 1; i <= ans; i++) {
+//                 sum = sum + i
+//             }
+//             console.log("The sum of n natural number is :", sum);
+//         }
+//         else {
+//             console.log("The given natural number should be greater than zero and non-negative");
+//         }
+//     }
+// }
+
+
+// Find the factorial of n natural numbers
+
+
+let promp = prompt("Enter your number: ")
+
+if (promp === null) {
+    console.log("Prompt cancelled!!");
 }
-else{
-    if (ans > 0) {
-        let sum = 0
-        for(i = 1; i<= ans ; i++) {
-            sum = sum + i
-            console.log("The sum of n natural number is :", sum);
+else {
+    let fc = Number(promp)
+
+    if (isNaN(fc)) {
+        console.log("The given number is invalid");
+    }
+    else {
+        if (fc > 0) {
+            let fact = 1 // The reason why we defined the zero is that in factorial we have to multiply the number with the next one unlike the sum
+            for (let i = 1; i <= fc; i++) {
+                fact = fact * i;
+            }
+            console.log("The Factorial of N number :", fact);
         }
-    }
-    else{
-        console.log("The given natural number should be greater than zero and non-negative");
-    }
+        else{
+            console.log("The given number should be greater than zero and also non-negative!");
+            
+        }
+
+    } 
 }
+
